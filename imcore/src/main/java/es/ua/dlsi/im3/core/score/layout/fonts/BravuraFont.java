7fdbb6569ddc089aba6ed0eee41d5d413f2f4c5b
David Rizo Valero <drizovalero@gmail.com>
2017-10-31 10:33:45 +0100

    Correct pom.xml to correctly load resources

--- imcore/src/main/java/es/ua/dlsi/im3/core/score/layout/fonts/BravuraFont.java
+++ imcore/src/main/java/es/ua/dlsi/im3/core/score/layout/fonts/BravuraFont.java
@@ -17,7 +17,8 @@ public class BravuraFont extends LayoutFont {
     private static final String SVGFONT = "/fonts/bravura/bravura_1.12.svg";
     private static final String METADATA = "/fonts/bravura/glyphnames-1.12.json";
     public BravuraFont() throws ImportException, IM3Exception {
-        super("Bravura", LayoutFonts.bravura, BravuraFont.class.getResourceAsStream(SVGFONT),
+        super("Bravura", LayoutFonts.bravura,
+                BravuraFont.class.getResourceAsStream(SVGFONT),
                 BravuraFont.class.getResourceAsStream(OTFMUSICFONT),
                 BravuraFont.class.getResourceAsStream(OTFTEXTCFONT),
                 BravuraFont.class.getResourceAsStream(METADATA),
