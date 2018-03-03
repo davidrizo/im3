88944bbb958caf49eb9a9d62112977220bcced78
David Rizo Valero <drizovalero@gmail.com>
2018-03-03 13:04:45 +0100

    Add some new "positions in staff" list values

--- imcore/src/main/java/es/ua/dlsi/im3/core/score/PositionsInStaff.java
+++ imcore/src/main/java/es/ua/dlsi/im3/core/score/PositionsInStaff.java
@@ -1,6 +1,11 @@
 package es.ua.dlsi.im3.core.score;
 
 public class PositionsInStaff {
+    public static final PositionInStaff FIRST_BOTTOM_LEDGER_LINE = new PositionInStaff(-2);
+    public static final PositionInStaff SECOND_BOTTOM_LEDGER_LINE = new PositionInStaff(-4);
+    public static final PositionInStaff THIRD_BOTTOM_LEDGER_LINE = new PositionInStaff(-6);
+    public static final PositionInStaff FOURTH_BOTTOM_LEDGER_LINE = new PositionInStaff(-8);
+
     /**
      * Bottom line
      */
@@ -16,6 +21,9 @@ public class PositionsInStaff {
      */
     public static final PositionInStaff LINE_5 = new PositionInStaff(8);
     public static final PositionInStaff FIRST_TOP_LEDGER_LINE = new PositionInStaff(10);
+    public static final PositionInStaff SECOND_TOP_LEDGER_LINE = new PositionInStaff(12);
+    public static final PositionInStaff THIRD_TOP_LEDGER_LINE = new PositionInStaff(14);
+    public static final PositionInStaff FOURTH_TOP_LEDGER_LINE = new PositionInStaff(16);
 
     /**
      * Bottom space
@@ -27,6 +35,10 @@ public class PositionsInStaff {
     public static final PositionInStaff SPACE_4 = new PositionInStaff(7);
     public static final PositionInStaff SPACE_5 = new PositionInStaff(9);
     public static final PositionInStaff SPACE_6 = new PositionInStaff(11);
+    public static final PositionInStaff SPACE_7 = new PositionInStaff(13);
+    public static final PositionInStaff SPACE_8 = new PositionInStaff(15);
     public static final PositionInStaff SPACE_MINUS_1 = new PositionInStaff(-3);
+    public static final PositionInStaff SPACE_MINUS_2 = new PositionInStaff(-5);
+    public static final PositionInStaff SPACE_MINUS_3 = new PositionInStaff(-7);
 }
 
