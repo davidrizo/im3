08904226116a07c088e1fb7d84d38679b9678d03
David Rizo Valero <drizovalero@gmail.com>
2018-06-11 16:57:37 +0200

    Change version

---
+++ muret/src/main/java/es/ua/dlsi/im3/omr/muret/IOMRSymbolBaseView.java
@@ -0,0 +1,10 @@
+package es.ua.dlsi.im3.omr.muret;
+
+import es.ua.dlsi.im3.omr.muret.model.OMRSymbol;
+
+/**
+ * @autor drizo
+ */
+public interface IOMRSymbolBaseView {
+    OMRSymbol getOMRSymbol();
+}
---
+++ muret/src/main/java/es/ua/dlsi/im3/omr/muret/Version.java
@@ -0,0 +1,8 @@
+package es.ua.dlsi.im3.omr.muret;
+
+/**
+ * @autor drizo
+ */
+public class Version {
+    public static String VERSION = "20180611";
+}
