c8e4d49345a2b7b5cf6fadc2b60fc94ba5fa3ffa
David Rizo Valero <drizovalero@gmail.com>
2018-09-12 18:55:55 +0200

    Add exporters

--- muret/src/main/java/es/ua/dlsi/im3/omr/muret/Model.java
+++ muret/src/main/java/es/ua/dlsi/im3/omr/muret/Model.java
@@ -2,11 +2,25 @@ package es.ua.dlsi.im3.omr.muret;
 
 
 import es.ua.dlsi.im3.core.IM3Exception;
+import es.ua.dlsi.im3.core.conversions.MensuralToModern;
+import es.ua.dlsi.im3.core.conversions.ScoreToPlayed;
 import es.ua.dlsi.im3.core.patternmatching.NearestNeighbourClassesRanking;
+import es.ua.dlsi.im3.core.played.PlayedSong;
+import es.ua.dlsi.im3.core.played.io.MidiSongExporter;
+import es.ua.dlsi.im3.core.score.Clef;
+import es.ua.dlsi.im3.core.score.Intervals;
 import es.ua.dlsi.im3.core.score.NotationType;
+import es.ua.dlsi.im3.core.score.ScoreSong;
+import es.ua.dlsi.im3.core.score.io.kern.KernExporter;
+import es.ua.dlsi.im3.core.score.io.lilypond.LilypondExporter;
+import es.ua.dlsi.im3.core.score.io.mei.MEISongExporter;
+import es.ua.dlsi.im3.core.score.io.musicxml.MusicXMLExporter;
+import es.ua.dlsi.im3.core.score.layout.AutomaticPageLayout;
+import es.ua.dlsi.im3.core.score.layout.CoordinateComponent;
 import es.ua.dlsi.im3.core.score.layout.DiplomaticLayout;
 import es.ua.dlsi.im3.core.score.layout.ScoreLayout;
 import es.ua.dlsi.im3.core.score.layout.coresymbols.LayoutStaffSystem;
+import es.ua.dlsi.im3.core.score.layout.pdf.PDFExporter;
 import es.ua.dlsi.im3.gui.javafx.dialogs.ShowError;
 import es.ua.dlsi.im3.gui.javafx.dialogs.ShowMessage;
 import es.ua.dlsi.im3.omr.classifiers.endtoend.AgnosticSequenceRecognizer;
@@ -92,5 +106,107 @@ public class Model {
      */
     public DiplomaticLayout getDiplomaticScoreLayout(OMRInstrument instrumentHierarchical, OMRRegion owner) throws IM3Exception {
         return currentProject.get().getDiplomaticLayout(instrumentHierarchical, owner);
+    }
+
+    private void checkMensural() throws IM3Exception {
+        if (currentProject.get().getNotationType() != NotationType.eMensural) {
+            throw new IM3Exception("Notation type is not mensural, it is " + currentProject.get().getNotationType());
+        }
+    }
+    public void exportMensuralMEI(File file) throws IM3Exception {
+        checkMensural();
+        MEISongExporter exporter = new MEISongExporter();
+        exporter.exportSong(file, currentProject.get().getScoreSong());
+    }
+
+    public void exportMensuralMens(File file) throws IM3Exception {
+        checkMensural();
+        KernExporter exporter = new KernExporter();
+        exporter.exportSong(file, currentProject.get().getScoreSong()); //TODO MensExporter
+    }
+
+    public void exportMensuralDiplomaticPDF(File file) throws IM3Exception {
+        checkMensural();
+        PDFExporter exporter = new PDFExporter();
+        AutomaticPageLayout automaticPageLayout = new AutomaticPageLayout(currentProject.get().getScoreSong(), null, true,
+                new CoordinateComponent(1000.0), new CoordinateComponent(2000.0)); //TODO Tamaño seleccionable
+        exporter.exportLayout(file, automaticPageLayout);
+    }
+
+    public void exportMensuralEditorialPDF(File file) throws IM3Exception {
+        checkMensural();
+        //TODO versión editorial
+    }
+
+    private ScoreSong getModernTranslation() throws IM3Exception {
+        checkMensural();
+        //TODO Que se pueda guardar, que no se genere siempre
+        MensuralToModern mensuralToModern = new MensuralToModern(new Clef[] {}); //TODO estos valores que estén guardados
+        ScoreSong modern = mensuralToModern.convertIntoNewSong(currentProject.get().getScoreSong(), Intervals.UNISON_PERFECT); //TODO estos valores que estén guardados
+
+        return modern;
+    }
+
+    public void exportMensuralAndModernPDF(File file) throws IM3Exception {
+        checkMensural();
+        //TODO El merge se carga la versión mensural
+        MensuralToModern mensuralToModern = new MensuralToModern(new Clef[] {}); //TODO estos valores que estén guardados
+        mensuralToModern.merge(currentProject.get().getScoreSong(), getModernTranslation());
+        PDFExporter exporter = new PDFExporter();
+        AutomaticPageLayout automaticPageLayout = new AutomaticPageLayout(currentProject.get().getScoreSong(), null, true,
+                new CoordinateComponent(1000.0), new CoordinateComponent(2000.0)); //TODO Tamaño seleccionable
+        exporter.exportLayout(file, automaticPageLayout);
+    }
+
+    public void exportMensuralLilypond(File file) throws IM3Exception {
+        checkMensural();
+        LilypondExporter lilypondExporter = new LilypondExporter();
+        lilypondExporter.exportSong(file, currentProject.get().getScoreSong());
+    }
+
+    public void exportPDFWithImagesAndEditorialComments(File file) throws IM3Exception {
+        checkMensural();
+        //TODO
+    }
+
+    public void exportModernMusicXML(File file) throws IM3Exception {
+        ScoreSong modern = getModernTranslation();
+        MusicXMLExporter exporter = new MusicXMLExporter();
+        exporter.exportSong(file, modern);
+    }
+
+    public void exportModernMEI(File file) throws IM3Exception {
+        ScoreSong modern = getModernTranslation();
+        MusicXMLExporter exporter = new MusicXMLExporter();
+        exporter.exportSong(file, modern);
+    }
+
+    public void exportModernKern(File file) throws IM3Exception {
+        ScoreSong modern = getModernTranslation();
+        KernExporter exporter = new KernExporter();
+        exporter.exportSong(file, modern);
+    }
+
+    public void exportModernPDF(File file) throws IM3Exception {
+        ScoreSong modern = getModernTranslation();
+        PDFExporter exporter = new PDFExporter();
+        AutomaticPageLayout automaticPageLayout = new AutomaticPageLayout(currentProject.get().getScoreSong(), null, true,
+                new CoordinateComponent(1000.0), new CoordinateComponent(2000.0)); //TODO Tamaño seleccionable
+        exporter.exportLayout(file, automaticPageLayout);
+    }
+
+    public void exportModernLilypond(File file) throws IM3Exception {
+        ScoreSong modern = getModernTranslation();
+        LilypondExporter exporter = new LilypondExporter();
+        exporter.exportSong(file, modern);
+    }
+
+    public void exportModernMIDI(File file) throws IM3Exception {
+        ScoreSong modern = getModernTranslation();
+
+        ScoreToPlayed scoreToPlayed = new ScoreToPlayed();
+        PlayedSong played = scoreToPlayed.createPlayedSongFromScore(modern);
+        MidiSongExporter exporter = new MidiSongExporter();
+        exporter.exportSong(file, played);
     }
 }
--- muret/src/main/java/es/ua/dlsi/im3/omr/muret/MusicController.java
+++ muret/src/main/java/es/ua/dlsi/im3/omr/muret/MusicController.java
@@ -8,15 +8,18 @@ import es.ua.dlsi.im3.core.score.layout.HorizontalLayout;
 import es.ua.dlsi.im3.core.score.layout.ScoreLayout;
 import es.ua.dlsi.im3.core.score.layout.fonts.LayoutFonts;
 import es.ua.dlsi.im3.gui.interaction.ISelectable;
+import es.ua.dlsi.im3.gui.javafx.dialogs.OpenSaveFileDialog;
 import es.ua.dlsi.im3.gui.javafx.dialogs.ShowError;
 import es.ua.dlsi.im3.gui.score.javafx.ScoreSongView;
 import es.ua.dlsi.im3.omr.muret.model.IOMRBoundingBox;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Menu;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.transform.Scale;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -33,6 +36,10 @@ public class MusicController extends MuRETBaseController {
 
     @FXML
     ScrollPane scrollPaneImages;
+
+    @FXML
+    Menu menuExportMensural;
+
     private ScoreSongView scoreSongView;
 
     @Override
@@ -45,6 +52,8 @@ public class MusicController extends MuRETBaseController {
             Logger.getLogger(this.getClass().getName()).log(Level.WARNING, "Cannot load the score sheet", e);
             showError("Cannot load the score sheet", e);
         }
+
+        menuExportMensural.setDisable(MuRET.getInstance().getModel().getCurrentProject().getNotationType() != NotationType.eMensural);
     }
 
     public void loadScoreSongView() throws IM3Exception {
@@ -104,4 +113,190 @@ public class MusicController extends MuRETBaseController {
     public ISelectable next(ISelectable s) {
         return null;
     }
+
+    private File askExportFile(String formatDescription, String extension) {
+        OpenSaveFileDialog openSaveFileDialog = new OpenSaveFileDialog();
+        return openSaveFileDialog.saveFile(getRoot().getScene().getWindow(), "Export", formatDescription, extension);
+    }
+    @FXML
+    private void handleFileSave() {
+        try {
+            MuRET.getInstance().getModel().save();
+        } catch (Throwable e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Cannot save", e);
+            showError( "Cannot save", e);
+        }
+    }
+
+    @FXML
+    private void handleExportMensuralMEI() {
+        try {
+            File file = askExportFile("Mensural notation MEI", "mei");
+            if (file != null) {
+                MuRET.getInstance().getModel().exportMensuralMEI(file);
+            }
+        } catch (Throwable e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Cannot export", e);
+            showError( "Cannot export", e);
+        }
+    }
+
+    @FXML
+    private void handleExportMensuralMens() {
+        try {
+            File file = askExportFile("**mens", "mns");
+            if (file != null) {
+                MuRET.getInstance().getModel().exportMensuralMens(file);
+            }
+        } catch (Throwable e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Cannot export", e);
+            showError( "Cannot export", e);
+        }
+    }
+
+    @FXML
+    private void handleExportMensuralDiplomaticPDF() {
+        try {
+            File file = askExportFile("PDF with diplomatic edition", "pdf");
+            if (file != null) {
+                MuRET.getInstance().getModel().exportMensuralDiplomaticPDF(file);
+            }
+        } catch (Throwable e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Cannot export", e);
+            showError( "Cannot export", e);
+        }
+    }
+
+    @FXML
+    private void handleExportMensuralEditorialPDF() {
+        try {
+            File file = askExportFile("PDF with edited content", "pdf");
+            if (file != null) {
+                MuRET.getInstance().getModel().exportMensuralEditorialPDF(file);
+            }
+        } catch (Throwable e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Cannot export", e);
+            showError( "Cannot export", e);
+        }
+    }
+
+    @FXML
+    private void handleExportModernAndMensuralPDF() {
+        try {
+            File file = askExportFile("Export PDF of modern notation score aligned with mensural", "xml");
+            if (file != null) {
+                MuRET.getInstance().getModel().exportMensuralAndModernPDF(file);
+            }
+        } catch (Throwable e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Cannot export", e);
+            showError( "Cannot export", e);
+        }
+    }
+
+
+    @FXML
+    private void handleExportMensuralLilypond() {
+        try {
+            File file = askExportFile("Export mensural Lilypond", "ly");
+            if (file != null) {
+                MuRET.getInstance().getModel().exportMensuralLilypond(file);
+            }
+        } catch (Throwable e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Cannot export", e);
+            showError( "Cannot export", e);
+        }
+    }
+
+    @FXML
+    private void handleExportImagesWithEditorialComments() {
+        try {
+            File file = askExportFile("PDF with images and comments", "pdf");
+            if (file != null) {
+                MuRET.getInstance().getModel().exportPDFWithImagesAndEditorialComments(file);
+            }
+        } catch (Throwable e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Cannot export", e);
+            showError( "Cannot export", e);
+        }
+    }
+
+    @FXML
+    private void handleExportModernMusicXML() {
+        try {
+            File file = askExportFile("Export MusicXML of modern notation score", "xml");
+            if (file != null) {
+                MuRET.getInstance().getModel().exportModernMusicXML(file);
+            }
+        } catch (Throwable e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Cannot export", e);
+            showError( "Cannot export", e);
+        }
+    }
+
+    @FXML
+    private void handleExportModernMEI() {
+        try {
+            File file = askExportFile("Export MEI of modern notation score", "xml");
+            if (file != null) {
+                MuRET.getInstance().getModel().exportModernMEI(file);
+            }
+        } catch (Throwable e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Cannot export", e);
+            showError( "Cannot export", e);
+        }
+    }
+
+    @FXML
+    private void handleExportModernKern() {
+        try {
+            File file = askExportFile("Export **kern of modern notation score", "krn");
+            if (file != null) {
+                MuRET.getInstance().getModel().exportModernKern(file);
+            }
+        } catch (Throwable e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Cannot export", e);
+            showError( "Cannot export", e);
+        }
+    }
+
+    @FXML
+    private void handleExportModernPDF() {
+        try {
+            File file = askExportFile("Export PDF of modern notation score", "xml");
+            if (file != null) {
+                MuRET.getInstance().getModel().exportModernPDF(file);
+            }
+        } catch (Throwable e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Cannot export", e);
+            showError( "Cannot export", e);
+        }
+    }
+
+
+    @FXML
+    private void handleExportModernLilypond() {
+        try {
+            File file = askExportFile("Export Lilypond of modern notation score", "xml");
+            if (file != null) {
+                MuRET.getInstance().getModel().exportModernLilypond(file);
+            }
+        } catch (Throwable e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Cannot export", e);
+            showError( "Cannot export", e);
+        }
+    }
+
+    @FXML
+    private void handleExportModernMIDI() {
+        try {
+            File file = askExportFile("Export MIDI performance", "xml");
+            if (file != null) {
+                MuRET.getInstance().getModel().exportModernMIDI(file);
+            }
+        } catch (Throwable e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Cannot export", e);
+            showError( "Cannot export", e);
+        }
+    }
+
 }
