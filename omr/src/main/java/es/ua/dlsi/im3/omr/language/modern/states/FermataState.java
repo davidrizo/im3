8495e445fe5b6ff0164fcd5fca7683914aaabf50
davidrizo <picabia2a>
2020-02-21 17:55:38 +0100

    Recover FermataState

---
+++ omr/src/main/java/es/ua/dlsi/im3/omr/language/modern/states/FermataState.java
@@ -0,0 +1,16 @@
+package es.ua.dlsi.im3.omr.language.modern.states;
+
+import es.ua.dlsi.im3.core.adt.dfa.State;
+import es.ua.dlsi.im3.omr.encoding.agnostic.AgnosticSymbol;
+import es.ua.dlsi.im3.omr.language.OMRTransduction;
+import es.ua.dlsi.im3.omr.language.modern.states.OMRState;
+
+public class FermataState extends OMRState {
+    public FermataState(int number, String name) {
+        super(number, name);
+    }
+
+    public void onEnter(AgnosticSymbol token, State previousState, OMRTransduction transduction) {
+        //TODO Agregar a la partitura en futuras implementaciones
+    }
+}
